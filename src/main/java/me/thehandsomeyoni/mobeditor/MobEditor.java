1e947841bb1e23ade66c19a4f060a014be3375c6
Mr-Angel <cohen.omri31@gmail.com>
2022-04-03 14:53:59 +0300

    updates

--- src/main/java/me/thehandsomeyoni/mobeditor/MobEditor.java
+++ src/main/java/me/thehandsomeyoni/mobeditor/MobEditor.java
@@ -1,5 +1,6 @@
 package me.thehandsomeyoni.mobeditor;
 
+import me.thehandsomeyoni.mobeditor.listeners.MobInteractionEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MobEditor extends JavaPlugin {
